11ae92c1f1800bf1e883763178c8bcffe8f8ff48
zdotdev <www.ragaszedrick25@gmail.com>
2025-04-19 14:56:15 +0800

    Added new design for pins and routes

--- frontend/app/components/custom/map/schoolMap.tsx
+++ frontend/app/components/custom/map/schoolMap.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { MapContainer, TileLayer, useMap } from "react-leaflet";
+import { MapContainer, TileLayer, useMap, Marker } from "react-leaflet";
 import L from "leaflet";
 import "leaflet-routing-machine";
 import { useEffect, useState } from "react";
 import { Button } from "~/components/ui/button";
+import { MapPin } from "lucide-react";
 
 type RoutingControlType = L.Routing.Control;
 
@@ -15,21 +16,37 @@ const getStoredPoints = () => {
 
 const { pointA, pointB } = getStoredPoints();
 
+const createCustomIcon = (color: string) => {
+  return L.icon({
+    iconUrl: `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-${color}.png`,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowUrl:
+      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+    shadowSize: [41, 41],
+    shadowAnchor: [12, 41],
+  });
+};
+
+const redIcon = createCustomIcon("red");
+const yellowIcon = createCustomIcon("yellow");
+
 const CustomRouting: React.FC = () => {
   const map = useMap();
+
+  const startPoint = pointA
+    ? L.latLng(...(pointA.split(",").map(Number) as [number, number]))
+    : null;
+  const endPoint = pointB
+    ? L.latLng(...(pointB.split(",").map(Number) as [number, number]))
+    : null;
 
   useEffect(() => {
     if (!map) return;
     try {
       const routingControl: RoutingControlType = L.Routing.control({
-        waypoints: [
-          pointA
-        ? L.latLng(...(pointA.split(",").map(Number) as [number, number]))
-        : L.latLng(0, 0),
-          pointB
-        ? L.latLng(...(pointB.split(",").map(Number) as [number, number]))
-        : L.latLng(0, 0),
-        ],
+        waypoints: [startPoint || L.latLng(0, 0), endPoint || L.latLng(0, 0)],
         routeWhileDragging: true,
         lineOptions: {
           styles: [{ color: "#E3A817", weight: 4 }],
@@ -38,6 +55,9 @@ const CustomRouting: React.FC = () => {
         },
         show: true,
         showAlternatives: true,
+        createMarker: () => {
+          return null;
+        }, // Disable default markers
       }).addTo(map);
 
       return () => {
@@ -50,7 +70,12 @@ const CustomRouting: React.FC = () => {
     }
   }, [map]);
 
-  return null;
+  return (
+    <>
+      {startPoint && <Marker position={startPoint} icon={redIcon} />}
+      {endPoint && <Marker position={endPoint} icon={yellowIcon} />}
+    </>
+  );
 };
 
 const saveToLocalStorage = (a: string, b: string) => {
@@ -97,9 +122,12 @@ const SchoolMap: React.FC = () => {
             saveToLocalStorage(point_a, point_b);
           }}
         >
-          <div>
+          <div className="flex items-center">
             <label htmlFor="from" className="mr-2">
-              From:
+              <div className="flex gap-2">
+                <MapPin className=" fill-red-600" />
+                From:
+              </div>
             </label>
             <select
               name="from"
@@ -119,9 +147,11 @@ const SchoolMap: React.FC = () => {
               ))}
             </select>
           </div>
-          <div>
+          <div className="flex items-center">
             <label htmlFor="to" className="mr-2">
-              To:
+              <div className="flex gap-2">
+                <MapPin className=" fill-yellow-300" /> To:
+              </div>
             </label>
             <select
               name="to"
